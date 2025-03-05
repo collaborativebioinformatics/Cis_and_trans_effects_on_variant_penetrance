7bca9de18c74386d31b1ac0c7e46cf844969d903
Louison <Louison5@icloud.com>
2025-03-05 10:07:57 -0500

    update run_lmm.sh

---
+++ example_run_lmm.sh
@@ -0,0 +1,4 @@
+./run_analysis.sh "dataPrepScripts/outputFromPlink_local_ancestry_pcs.csv" \
+                  "testPlink/synthetic_small_v1.pheno1" \
+                  "Phenotype(liability)" \
+                  "dataPrepScripts/SNVoutputFromPlink.txt"
---
+++ run_lmm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Assign input arguments to variables
+HAP_P=$1
+PHE_P=$2
+SUBSET=$3
+SNV_P=$4
+
+# Run Python script with provided arguments
+python3 << EOF
+import pandas as pd
+import numpy as np
+from some_module import StructLMM  # Replace with actual import if needed
+
+# Assign variables
+HAP_P = "$HAP_P"
+PHE_P = "$PHE_P"
+SUBSET = "$SUBSET"
+SNV_P = "$SNV_P"
+
+# Load data
+HAPS = pd.read_csv(HAP_P, header=1, index_col=0)
+PHE = pd.read_csv(PHE_P, sep="\t")
+SNV = pd.read_csv(SNV_P, sep="\t")
+
+# Prepare data
+y = PHE[SUBSET].to_numpy()
+E = HAPS.iloc[:, 1:].to_numpy()
+M = np.concatenate([np.ones((E.shape[0], 1)), E], axis=1)
+g = SNV.iloc[:, 1].to_numpy()
+
+# Run analysis
+lmm = StructLMM(y, M, E)
+lmm.fit()
+lmm.score_2dof_inter(g)
+EOF
